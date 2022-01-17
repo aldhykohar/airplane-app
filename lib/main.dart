d758fe0fedac8af8e7ea8201587e18ca6309326e
aldhykohar <aldhykohar@stimednp.ac.id>
2022-01-18 07:49:19 +0800

    :zap: create route slash to get started

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:airplane/ui/pages/get_started_page.dart';
+import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
-import 'ui/pages/get_started_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,8 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GetStartedPage(),
+      home: SplashPage(),
+      routes: {
+        '/splash': (context) => SplashPage(),
+        '/get-started':(context)=>GetStartedPage()
+      },
     );
   }
 }
-
--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,9 +1,26 @@
+import 'dart:async';
+
+import 'package:airplane/ui/pages/get_started_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../../shared/theme.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushNamed(context, '/get-started');
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +38,11 @@ class SplashPage extends StatelessWidget {
                   image: DecorationImage(
                       image: AssetImage('assets/icon_plan.png'))),
             ),
-            Text('AIRPLANE',
-            style: whiteTextStyle.copyWith(
-              fontSize: 32,
-              fontWeight: medium,
-              letterSpacing: 10
-            ),)
+            Text(
+              'AIRPLANE',
+              style: whiteTextStyle.copyWith(
+                  fontSize: 32, fontWeight: medium, letterSpacing: 10),
+            )
           ],
         ),
       ),
