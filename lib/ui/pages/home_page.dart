0f4e4d4611bff5ff65a5e711ed22a9005507392e
aldhykohar <aldhykohar@stimednp.ac.id>
2022-01-19 20:41:03 +0800

    :zap: create card destination widget

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/widget/destination_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,8 +7,91 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home Page'),
+    Widget header() {
+      return Container(
+        margin:
+            EdgeInsets.only(left: defaultMargin, top: 30, right: defaultMargin),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hello,\nJhon Wick',
+                    overflow: TextOverflow.ellipsis,
+                    style: blackTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    'Where to fly today?',
+                    style:
+                        greyTextStyle.copyWith(fontSize: 16, fontWeight: light),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage('assets/image_profile.png'))),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget popularDestinations() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              DestinationCard(
+                title: 'Lake Ciliwung',
+                city: 'Tangerang',
+                imageUrl: 'assets/image_destination1.png',
+                rating: 4.8,
+              ),
+              DestinationCard(
+                title: 'White Houses',
+                city: 'Spain',
+                imageUrl: 'assets/image_destination2.png',
+                rating: 4.7,
+              ),
+              DestinationCard(
+                title: 'Hill Heyo',
+                city: 'Monaco',
+                imageUrl: 'assets/image_destination3.png',
+                rating: 4.8,
+              ),
+              DestinationCard(
+                title: 'Menarra',
+                city: 'Japan',
+                imageUrl: 'assets/image_destination4.png',
+                rating: 5.0,
+              ),
+              DestinationCard(
+                title: 'Payung Teduh',
+                city: 'Singapore',
+                imageUrl: 'assets/image_destination5.png',
+                rating: 4.8,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return ListView(
+      children: [header(), popularDestinations()],
     );
   }
 }
