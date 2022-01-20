d05622d22b4b490b90ffcc3d8b3e0c76adf487d5
aldhykohar <aldhykohar@stimednp.ac.id>
2022-01-21 00:36:55 +0800

    :zap: add cubit to bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/ui/pages/bonus_page.dart';
 import 'package:airplane/ui/pages/choose_seat_page.dart';
 import 'package:airplane/ui/pages/get_started_page.dart';
@@ -5,6 +6,7 @@ import 'package:airplane/ui/pages/main_page.dart';
 import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,16 +15,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashPage(),
-      routes: {
-        '/splash': (context) => SplashPage(),
-        '/get-started': (context) => GetStartedPage(),
-        '/sign-up': (context) => SignUpPage(),
-        '/bonus': (context) => BonusPage(),
-        '/main': (context) => MainPage(),
-      },
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => PageCubit())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashPage(),
+        routes: {
+          '/splash': (context) => SplashPage(),
+          '/get-started': (context) => GetStartedPage(),
+          '/sign-up': (context) => SignUpPage(),
+          '/bonus': (context) => BonusPage(),
+          '/main': (context) => MainPage(),
+        },
+      ),
     );
   }
 }
