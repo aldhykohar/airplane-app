c59b91c08ed84e10d1a7429b06d13c95d3d6d3ed
aldhykohar <aldhykohar@stimednp.ac.id>
2022-01-22 10:28:58 +0800

    :zap: create sign out fitur

--- lib/cubit/auth_cubit.dart
+++ lib/cubit/auth_cubit.dart
@@ -8,21 +8,30 @@ part 'auth_state.dart';
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
 
-  void signUp({required String email,
-    required String password,
-    required String name,
-    String hobby = ''}) async {
+  void signUp(
+      {required String email,
+      required String password,
+      required String name,
+      String hobby = ''}) async {
     try {
       emit(AuthLoading());
 
-      UserModel user = await AuthService().signUp(
-          email: email, password: password, name: name, hobby: hobby);
+      UserModel user = await AuthService()
+          .signUp(email: email, password: password, name: name, hobby: hobby);
 
       emit(AuthSuccess(user));
     } catch (e) {
       emit(AuthFailed(e.toString()));
     }
   }
+
+  void signOut() async {
+    try {
+      emit(AuthLoading());
+      await AuthService().signOut();
+      emit(AuthInitial());
+    } catch (e) {
+      emit(AuthFailed(e.toString()));
+    }
+  }
 }
-
-
