209add19207196aa4cdbb9ddb0d4bb1baf0802df
aldhykohar <aldhykohar@stimednp.ac.id>
2022-01-24 11:11:58 +0800

    :zap: handle choose seat page

--- lib/ui/pages/detail_page.dart
+++ lib/ui/pages/detail_page.dart
@@ -241,7 +241,8 @@ class DetailPage extends StatelessWidget {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => ChooseSeatPage()));
+                              builder: (context) =>
+                                  ChooseSeatPage(destinationModel)));
                     },
                     width: 170,
                   )
